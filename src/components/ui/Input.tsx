20397af5e803722451974a4b446c70b4492ef930
정유진 <jane320@naver.com>
2025-07-23 15:27:16 +0900

    [ feat ] 공통 FormInput 컴포넌트 추가 (#1)

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@/utils/cn';
+import * as React from 'react';
+
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.ComponentProps<'input'>
+>(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        'flex h-10 w-full rounded-md bg-background border px-3 py-2 text-base placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        className,
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Input.displayName = 'Input';
+
+export { Input };
