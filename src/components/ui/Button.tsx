a2986b90da83dcbae4d5a55066e38997ba50c136
정유진 <jane320@naver.com>
2025-07-24 21:02:40 +0900

    [ feat ] 공통 Button 컴포넌트 추가 (#1)

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { cn } from '@/utils/cn';
+import {
+  cva,
+  type VariantProps,
+} from 'class-variance-authority';
+
+type ButtonProps =
+  React.ButtonHTMLAttributes<HTMLButtonElement> &
+    VariantProps<typeof buttonVariants>;
+
+const buttonVariants = cva(
+  'flex justify-center items-center text-sm disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default: 'bg-primary text-primary-foreground',
+      },
+      size: {
+        default: 'h-[36px] px-[16px] py-[5px]',
+        sm: 'h-[32px] px-[12px] test-xs',
+        lg: 'h-[40px] px-[20px]',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+);
+
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  ButtonProps
+>(({ className, type, variant, size, ...props }, ref) => {
+  return (
+    <button
+      type={type ?? 'button'}
+      className={cn(
+        buttonVariants({ variant, size, className }),
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
+Button.displayName = 'Button';
+
+export { Button };
