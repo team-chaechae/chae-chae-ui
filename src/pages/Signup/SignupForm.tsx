27381945dfb0ef99949e764ee32825a9efde147a
정유진 <jane320@naver.com>
2025-07-25 20:45:38 +0900

    [ feat ] 회원가입 API 연결 및 API 연동에 필요한 공통 Fetch 함수 생성

--- src/pages/Signup/SignupForm.tsx
+++ src/pages/Signup/SignupForm.tsx
@@ -16,6 +16,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { signupSchema } from '@/schemas/signupSchema';
 import { FormSelect } from '@/components/FormField/FormSelect';
 import { jobTitleOptions } from '@/constants/selectOptions';
+import { fetchClient } from '@/services/fetchClient';
+import { useNavigate } from 'react-router-dom';
 
 type FormValues = z.infer<typeof signupSchema>;
 export const SignUpForm = () => {
@@ -30,16 +32,43 @@ export const SignUpForm = () => {
     },
     resolver: zodResolver(signupSchema),
   });
-  const onSubmit: SubmitHandler<FormValues> = (
+  const navigate = useNavigate();
+  const errorMessage =
+    methods.formState.errors.root?.message ?? '';
+
+  const onSubmit: SubmitHandler<FormValues> = async (
     data: FormValues,
   ) => {
-    console.log('data', data);
+    const request = {
+      internalUser: {
+        email: data.email,
+        password: data.password,
+        realName: data.name,
+        employeeCode: data.employeeId,
+        position: data.jobTitle,
+      },
+    };
+    try {
+      await fetchClient({
+        method: 'POST',
+        endpoint: '/users/signup',
+        data: request,
+      });
+      navigate('/login');
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error(error.message);
+        methods.setError('root', {
+          message: error.message,
+        });
+      }
+    }
   };
 
   return (
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit)}>
-        <div className='flex flex-col gap-5 w-[320px]'>
+        <div className='flex flex-col gap-3 w-[320px]'>
           <FormInput
             name='email'
             type='email'
@@ -92,6 +121,11 @@ export const SignUpForm = () => {
             items={jobTitleOptions}
             showErrorMessage
           />
+          {errorMessage && (
+            <p className='text-xs text-red-400'>
+              {errorMessage}
+            </p>
+          )}
           <Button className='w-full mt-5' type='submit'>
             회원가입
           </Button>
