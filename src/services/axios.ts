41344ce18871d7113a8ed4b451e7c8d606d216af
정유진 <jane320@naver.com>
2025-07-29 21:56:13 +0900

    [ feat ] API 요청 시 헤더에 액세스 토큰 자동 추가 및 재발급 로직 구현 (#2)

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,4 +4,37 @@ export const axiosInstance = axios.create({
   baseURL: 'http://localhost:8080/api',
   timeout: 3000,
   headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      config.headers.set?.(
+        'Authorization',
+        `Bearer ${token}`,
+      );
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    const newAccessToken =
+      response.headers['authorization'];
+    if (newAccessToken?.startsWith('Bearer ')) {
+      const token = newAccessToken.replace('Bearer ', '');
+      localStorage.setItem('access_token', token);
+    }
+
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
