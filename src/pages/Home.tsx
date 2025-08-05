95b0872b4c5529597cc22ed1dae8f6d50323d991
정유진 <jane320@naver.com>
2025-08-05 23:52:09 +0900

    [ feat ] 메인 화면 달력 추가 및 스타일 수정 (#5)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,14 @@
+import { Calendar } from '@/components/ui/Calendar';
+
 export const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className='flex flex-col gap-5'>
+      <div className='border-b h-28'></div>
+      <div className='flex px-5 py-10'>
+        <div className='flex-1'>
+          <Calendar />
+        </div>
+      </div>
+    </div>
+  );
 };
