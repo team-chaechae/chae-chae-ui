5052faa1ae25fc3141046873c7a6309a9f9a1d2f
정유진 <jane320@naver.com>
2025-08-05 23:56:40 +0900

    [ feat ] 메인 화면 공지사항 기본 UI 구현 (#5)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,31 @@ export const Home = () => {
         <div className='flex-1'>
           <Calendar />
         </div>
+        <div className='flex-1 bg-gray-200 rounded-md shadow p-4'>
+          <h2 className='text-lg font-semibold mb-4 text-gray-900'>
+            📢 공지사항
+          </h2>
+          <ul className='space-y-3 text-sm text-gray-700'>
+            <li className='border-b pb-2 hover:underline cursor-pointer'>
+              [점검] 시스템 점검 안내 (8/10 02:00~04:00)
+            </li>
+            <li className='border-b pb-2 hover:underline cursor-pointer'>
+              [신규 기능] 재고 통합 관리 기능 출시
+            </li>
+            <li className='border-b pb-2 hover:underline cursor-pointer'>
+              [이벤트] 여름 맞이 배송비 할인 (~8/31)
+            </li>
+            <li className='border-b pb-2 hover:underline cursor-pointer'>
+              [점검] 시스템 점검 안내 (8/10 02:00~04:00)
+            </li>
+            <li className='border-b pb-2 hover:underline cursor-pointer'>
+              [신규 기능] 재고 통합 관리 기능 출시
+            </li>
+            <li className='border-b pb-2 hover:underline cursor-pointer'>
+              [이벤트] 여름 맞이 배송비 할인 (~8/31)
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
